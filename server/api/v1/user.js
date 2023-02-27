99db0c95c57b843c6327f753284d9928e3b2a5cb
Avinash verma <avinashverma165@gmail.com>
2023-02-27 13:11:28 +0530

    Auth Api
    Not yet completed, some error still to be resolved.

--- server/api/v1/user.js
+++ server/api/v1/user.js
@@ -1,10 +1,21 @@
-import { serverSupabaseClient } from "#supabase/server";
+import {createClient} from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://cujuxkdkpgxdhybcppxz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1anV4a2RrcGd4ZGh5YmNwcHh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU4OTU4OTgsImV4cCI6MTk5MTQ3MTg5OH0.V9Bjh9HHtvzu-cblE0Q0tNKxoGS17pRmvx1OtDlvB9Q'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 export default eventHandler(async (event) => {
-  const client = serverSupabaseClient(event);
-  const { data } = await client
-    .from("profiles")
-    .select("id, username, full_name, avatar_url, website")
-    .eq("username", "albert")
-    .limit(1);
-  return data;
-});
+  const { data: users, error } = await supabase.auth.signInWithOAuth()
+  if (error) {
+    console.error(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to retrieve user list' })
+    }
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(users)
+    }
+  }
+})
